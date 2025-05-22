31a0f3aef9677682d11809f11a91b5b2d56e91e3
AaronSchroederMi <Aaron-schroeder-mi@web.de>
2025-05-22 13:13:28 +0200

    update Main#resources to use Streams
    gives null if ressource was not found

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -86,31 +87,17 @@ public class Main {
      * @return String of all matching lines, separated by {@code "\n"}
      */
     public static String resources(String path) {
-        // TODO
-        StringBuilder result = new StringBuilder();
-
         try (InputStream stream = getResourceAsStream(path)) {
             BufferedReader r = new BufferedReader(new InputStreamReader(stream));
-
-            List<String> allLines = new ArrayList<>();
-
-            String newLine = r.readLine();
-            while (newLine != null) {
-                allLines.add(newLine);
-                newLine = r.readLine();
-            }
-
-            for (int i = 1; i < allLines.size(); i++) {
-                String s = allLines.get(i);
-                if (s.startsWith("a") && !(s.length() < 2)) {
-                    result.append(allLines.get(i)).append("\n");
-                }
-            }
-
+            return r.lines().
+                filter(s -> s.length() >= 2).
+                filter(s -> s.startsWith("a")).
+                map(s -> s + "\n").
+                collect(Collectors.joining());
         } catch (IOException e) {
             System.err.println("Ouch, that didn't work: \n" + e.getMessage());
         }
-
-        return result.toString();
+        //on failure
+        return null;
     }
 }
